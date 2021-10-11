530a1f24745d71113cff75943e342d1e0054a6b1
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-10-11 19:47:52 -0300

    Criando a lista de funcionario.txt

--- ManipulacaoArquivo/src/controller/Arquivo.java
+++ ManipulacaoArquivo/src/controller/Arquivo.java
@@ -4,12 +4,41 @@ package controller;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import model.bean.Funcionario;
+import model.bean.Pessoa;
 
 
 public class Arquivo {
     public static void main(String[] args) throws IOException{
         
-        File arquivo = new File("C:\\Users\\Josiene\\Documents\\NetBeansProjects\\ManipulacaoArquivo\\ManipulacaoArquivo\\src\\armazenaArquivosTXT\\arquivo.txt");
+        Funcionario atendente = new Funcionario();
+        
+        atendente.setCpf("397.945.603-08");
+        atendente.setNome("Isabelle Eloá Mirella Porto");
+        atendente.setEmail("iisabelleeloamirellaporto@agaxtur.com.br");
+        atendente.setIdade("25");
+        atendente.setSalario("R$ 1.850,00");
+        atendente.setHorasExtras(null);
+        
+        Funcionario engenheiro = new Funcionario();
+        
+        engenheiro.setCpf("754.415.387-83");
+        engenheiro.setNome("Sebastião Levi Augusto Assunção");
+        engenheiro.setEmail("sebastiaoleviaugustoassuncao__sebastiaoleviaugustoassuncao@portoweb.com.br");
+        engenheiro.setIdade("50");
+        engenheiro.setSalario("R$ 6.000,00");
+        engenheiro.setHorasExtras(null);
+        
+        List<Funcionario> listaFuncionario = new ArrayList<Funcionario>();
+        
+        listaFuncionario.add(atendente);
+        listaFuncionario.add(engenheiro);
+        
+        
+        
+        File arquivo = new File("C:\\Users\\Josiene\\Documents\\NetBeansProjects\\ManipulacaoArquivo\\ManipulacaoArquivo\\src\\armazenaArquivosTXT\\listaFuncionario.txt");
         
         if(!arquivo.exists()){
             arquivo.createNewFile();
@@ -17,8 +46,17 @@ public class Arquivo {
         
         FileWriter escreverArquivo = new FileWriter(arquivo);
         
-        for(int posicao = 1;posicao <= 10;posicao++){
-            escreverArquivo.write("Minha "+posicao+"º linha \n");
+        escreverArquivo.write("------------- Lista de Funcionários ------------- \n");
+        
+        for(Funcionario f:listaFuncionario){
+            escreverArquivo.write("\n");
+            escreverArquivo.write("CPF: "+f.getCpf()+"\n");
+            escreverArquivo.write("Nome: "+f.getNome()+"\n");
+            escreverArquivo.write("Email: "+f.getEmail()+"\n");
+            escreverArquivo.write("Idade: "+f.getIdade()+" anos \n");
+            escreverArquivo.write("Salário: "+f.getSalario()+"\n");
+            escreverArquivo.write("Horas Extras: "+f.getHorasExtras()+"\n");
+            escreverArquivo.write("----------------------------------------");
         }
         
         
