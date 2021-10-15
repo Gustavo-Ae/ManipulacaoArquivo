630ecb9f01ab98afee59006c9df95d515844d420
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-10-15 19:03:57 -0300

    lendo o arquivo separado por ponto-virgula

--- ManipulacaoArquivo/src/controller/LerArquivo.java
+++ ManipulacaoArquivo/src/controller/LerArquivo.java
@@ -4,7 +4,11 @@ package controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import model.bean.Funcionario;
+import model.bean.Pessoa;
 
 
 public class LerArquivo {
@@ -14,14 +18,34 @@ public class LerArquivo {
                 
         Scanner lerArquivo = new Scanner(entradaArquivo,"UTF-8");
         
+        List<Funcionario> listaFuncionario = new ArrayList<Funcionario>();
+        
         while(lerArquivo.hasNext()){ 
             
             String linha = lerArquivo.nextLine();
   
             if(!linha.isEmpty()){
-                System.out.println(linha);
-            }
+                String[] dados = linha.split("\\;");
+                Funcionario funcionario = new Funcionario();
 
+                funcionario.setCpf(dados[0]);
+                funcionario.setNome(dados[1]);
+                funcionario.setEmail(dados[2]);
+                funcionario.setIdade(dados[3]);
+                funcionario.setSalario(dados[4]);
+                if(dados[5].contains("null")){
+                    funcionario.setHorasExtras(null);
+                }else{
+                    funcionario.setHorasExtras(Integer.parseInt(dados[5]));
+                }
+                
+                listaFuncionario.add(funcionario);
+            }
         }
+        
+        for(Funcionario funcionario:listaFuncionario){
+            System.out.println(funcionario);
+        }
+        
     }
 }
