b94f5b5fc06fcf8197cdf0f83a28e231dac72686
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-11-01 20:10:39 -0300

    Editando Célula existente no Excel

--- ManipulacaoArquivo/src/controller/EditandoApachePOI.java
+++ ManipulacaoArquivo/src/controller/EditandoApachePOI.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Iterator;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -28,12 +29,24 @@ public class EditandoApachePOI{
         while(linhaInterator.hasNext()){
             Row linha = linhaInterator.next(); /*dados do funcionario na linha*/
             
-            int numeroCelulas = linha.getPhysicalNumberOfCells();
-            
+             int numeroCelulas = linha.getPhysicalNumberOfCells();
+
             Cell cell = linha.createCell(numeroCelulas);  /*Cria nova celula nas 2 linhas */
-            cell.setCellValue("disponivel");
-        }
+            cell.setCellValue("disponivel"); 
+        } 
         
+        int respostaAlterarHoraExtra = JOptionPane.showConfirmDialog(null, "Deseja alterar o salário dos funcionários ?","Pergunta",JOptionPane.YES_NO_OPTION);
+        
+        if(respostaAlterarHoraExtra == 0){ //SIM
+ 
+            for(int i = 0; i < planilha.getPhysicalNumberOfRows();i++){
+                Row linha = planilha.getRow(i);
+                
+                String perguntaSalario = JOptionPane.showInputDialog(null,"Qual o valor do novo salario do "+linha.getCell(1)+" ?","Pergunta",JOptionPane.QUESTION_MESSAGE);
+
+                linha.getCell(4).setCellValue("R$ "+perguntaSalario+",00");
+            }
+        }
         entrada.close();
         
         FileOutputStream saida = new FileOutputStream(arquivo);
