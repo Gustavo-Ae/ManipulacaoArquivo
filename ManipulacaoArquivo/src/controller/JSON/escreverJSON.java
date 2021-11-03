f17a749f71f591d6cf15183a6698a74f9a431b08
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-11-03 18:44:13 -0300

    Escrevendo JSON com GSON

---
+++ ManipulacaoArquivo/src/controller/JSON/escreverJSON.java
@@ -0,0 +1,48 @@
+package controller.JSON;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import model.bean.Funcionario;
+
+
+
+public class escreverJSON {
+    public static void main(String[] args) throws IOException {
+        
+        Funcionario atendente = new Funcionario();
+        
+        atendente.setCpf("397.945.603-08");
+        atendente.setNome("Isabelle Eloá Mirella Porto");
+        atendente.setEmail("iisabelleeloamirellaporto@agaxtur.com.br");
+        atendente.setIdade("25");
+        atendente.setSalario("R$ 1.850,00");
+        atendente.setHorasExtras(null);
+        
+        Funcionario engenheiro = new Funcionario();
+        
+        engenheiro.setCpf("754.415.387-83");
+        engenheiro.setNome("Sebastião Levi Augusto Assunção");
+        engenheiro.setEmail("sebastiaoleviaugustoassuncao__sebastiaoleviaugustoassuncao@portoweb.com.br");
+        engenheiro.setIdade("50");
+        engenheiro.setSalario("R$ 6.000,00");
+        engenheiro.setHorasExtras(null);
+        
+        List<Funcionario> listaFuncionario = new ArrayList<Funcionario>();
+        
+        listaFuncionario.add(atendente);
+        listaFuncionario.add(engenheiro);
+        
+        String jsonUsuarios = new Gson().toJson(listaFuncionario); //passa para JSON
+        
+        System.out.println(jsonUsuarios);
+        
+        FileWriter escreverArquivo = new FileWriter("C:\\Users\\Josiene\\Documents\\NetBeansProjects\\ManipulacaoArquivo\\ManipulacaoArquivo\\src\\armazenaArquivos\\fileJson.json");
+        
+        escreverArquivo.write(jsonUsuarios);
+        escreverArquivo.flush();
+        escreverArquivo.close();
+    }
+}
